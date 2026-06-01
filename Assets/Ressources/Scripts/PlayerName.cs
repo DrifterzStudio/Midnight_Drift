4a8b24ba7ee911f23789e00a95ea3366597cfe70
unknown <ablanchet@gamingcampus.local>
2026-06-01 14:23:39 +0200

    [Kroktur] tien tom ! :)

--- Assets/Ressources/Scripts/PlayerName.cs
+++ Assets/Ressources/Scripts/PlayerName.cs
@@ -1,23 +1,47 @@
 using Mirror;
+using Steamworks;
 using TMPro;
 using UnityEngine;
 
 public class PlayerName : NetworkBehaviour
 {
-    [SerializeField] private TMP_Text text;
+    [SerializeField] private TMP_Text nameTagGui;
+    private string nameTag = string.Empty;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
     }
-    public override void OnStartLocalPlayer()
+    public override void OnStartClient()
     {
-        base.OnStartLocalPlayer();
-        text.gameObject.SetActive(false);
+        base.OnStartClient();
+        nameTag = SteamFriends.GetPersonaName();
+        if (isLocalPlayer)
+        {
+            nameTagGui.gameObject.SetActive(false);
+        }
+        else
+        {
+            nameTagGui.SetText(nameTag);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void LateUpdate()
+    {
+        if (Camera.main != null && !isLocalPlayer)
+        {
+            Vector3 direction = Camera.main.transform.position - nameTagGui.transform.position;
+            direction.y = 0;
+
+            if (direction != Vector3.zero)
+            {
+                nameTagGui.transform.rotation = Quaternion.LookRotation(-direction);
+            }
+        }
     }
 }
