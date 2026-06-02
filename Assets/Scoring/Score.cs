d5d67520a727708d6bc830f327dfe8540a4f285b
Brenda-Inocencio <binocencio@gaming.tech>
2026-06-02 15:04:15 +0200

    Update score & bugs fixes

--- Assets/Scoring/Score.cs
+++ Assets/Scoring/Score.cs
@@ -8,25 +8,24 @@ using UnityEngine.UI;
 public class Score : RCCP_GenericComponent  {
     private RCCP_CarController carController;
 
-    private int metters = 0;
-    private float score = 0;
-    private int multiplier = 1;
+    private int metters = 0; private float score = 0;
     private int multiplierModifier = 1;
-    private int timer = 0;
-    private float challengeMultiplier = 1;
+    private int frames = 0;
+
+    private float multiplier = 1; private float challengeMultiplier = 1;
+
     private bool[] scoreAchievements = { false, false, false }; // 10000 / 5000 / 1000
     private bool[] distAchievements = { false, false, false }; // 200 / 100 / 50
     private bool[] parkingChallenge = { false, false }; // dist 30m / 5000 pts
+
     private bool isChallengeMultAply = false;
     private bool isEnd = false;
-    
    
     [Tooltip("Text showing the current score.")]
     [Space()]
     public Text scoreText;
 
     private void Update() {
-        
         // Getting active player car controller on the scene.
         carController = RCCPSceneManager.activePlayerVehicle;
          
@@ -35,38 +34,37 @@ public class Score : RCCP_GenericComponent  {
             return;
 
         if (!isEnd) {
-
             // Calculate the score when drifting.
-            if (carController.handbrakeInput_V == 1f && carController.speed >= 0 && carController.steerInput_V != 0) {
-                timer++;
+            if (carController.speed >= 0 && carController.steerInput_V != 0 && carController.GetVehicleBehaviorType().driftMode) {
+                frames++;
                 metters += (int)Math.Abs(carController.speed) * 10 / 36; // km/h to m/s
-                metters /= timer;
+                metters /= frames;
 
                 // Calculate the score achievements multiplier.
                 if (score >= 10000 && !scoreAchievements[0]) {
-                    multiplier += 7;
+                    multiplier += 2.5f;
                     scoreAchievements[0] = true;
                 }
                 if (score >= 5000 && !scoreAchievements[1]) {
-                    multiplier += 5;
+                    multiplier += 2;
                     scoreAchievements[1] = true;
                 }
                 if (score >= 1000 && !scoreAchievements[2]) {
-                    multiplier += 3;
+                    multiplier += 1.5f;
                     scoreAchievements[2] = true;
                 }
 
                 // Calculate the distance achievements multiplier.
                 if (metters >= 200 && !distAchievements[0]) {
-                    multiplier += 5;
+                    multiplier += 2.5f;
                     distAchievements[0] = true;
                 }
                 if (metters >= 100 && !distAchievements[1]) {
-                    multiplier += 3;
+                    multiplier += 2;
                     distAchievements[1] = true;
                 }
                 if (metters >= 50 && !distAchievements[2]) {
-                    multiplier += 2;
+                    multiplier += 1.5f;
                     distAchievements[2] = true;
                 }
 
@@ -89,7 +87,7 @@ public class Score : RCCP_GenericComponent  {
                 }
             }
             else {
-                timer = 0;
+                frames = 0;
                 metters = 0;
                 multiplierModifier = 0;
             }
