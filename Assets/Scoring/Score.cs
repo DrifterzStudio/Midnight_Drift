82ea45f3196aff0b949a394275cb34fde0a8d1bf
Brenda-Inocencio <binocencio@gaming.tech>
2026-06-03 11:01:12 +0200

    Update score

--- Assets/Scoring/Score.cs
+++ Assets/Scoring/Score.cs
@@ -9,8 +9,7 @@ public class Score : RCCP_GenericComponent  {
     private RCCP_CarController carController;
 
     private int metters = 0; private float score = 0;
-    private int multiplierModifier = 1;
-    private int frames = 0;
+    private int multiplierModifier = 1; private int distDrift = 0;
 
     private float multiplier = 1; private float challengeMultiplier = 1;
 
@@ -35,10 +34,10 @@ public class Score : RCCP_GenericComponent  {
 
         if (!isEnd) {
             // Calculate the score when drifting.
-            if (carController.speed >= 0 && carController.steerInput_V != 0 && carController.GetVehicleBehaviorType().driftMode) {
-                frames++;
-                metters += (int)Math.Abs(carController.speed) * 10 / 36; // km/h to m/s
-                metters /= frames;
+            if ((float)Math.Abs(carController.PoweredAxles[0].leftWheelCollider.SidewaysSlip) >= 0.25f && 
+                carController.GetVehicleBehaviorType().driftMode) {
+
+                metters += ((int)Math.Abs(carController.speed) * 100 / 3600);
 
                 // Calculate the score achievements multiplier.
                 if (score >= 10000 && !scoreAchievements[0]) {
@@ -56,15 +55,15 @@ public class Score : RCCP_GenericComponent  {
 
                 // Calculate the distance achievements multiplier.
                 if (metters >= 200 && !distAchievements[0]) {
-                    multiplier += 2.5f;
+                    multiplier += 1.8f;
                     distAchievements[0] = true;
                 }
                 if (metters >= 100 && !distAchievements[1]) {
-                    multiplier += 2;
+                    multiplier += 1.2f;
                     distAchievements[1] = true;
                 }
                 if (metters >= 50 && !distAchievements[2]) {
-                    multiplier += 1.5f;
+                    multiplier += 1;
                     distAchievements[2] = true;
                 }
 
@@ -78,21 +77,22 @@ public class Score : RCCP_GenericComponent  {
                      parkingChallenge[1] = true;
                  }
 
+                distDrift += metters;
                 // Calculate the score multiplier.
-                if (multiplierModifier < metters) {
-                    if (multiplierModifier + 100 <= metters) {
+                if (multiplierModifier < distDrift) {
+                    if (multiplierModifier + 100 <= distDrift) {
                         multiplierModifier += 100;
-                        multiplier += multiplierModifier / 100;
+                        multiplier += multiplierModifier / 500;
                     }
                 }
+                score += metters * multiplier;
+                metters = 0;
             }
             else {
-                frames = 0;
-                metters = 0;
+                distDrift = 0;
                 multiplierModifier = 0;
             }
 
-            score += metters * multiplier;
         }
         else if (isEnd && !isChallengeMultAply) {
             score *= challengeMultiplier;
