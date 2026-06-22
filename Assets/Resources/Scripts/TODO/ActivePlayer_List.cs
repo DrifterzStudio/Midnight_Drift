27b650221a8d3804bf2d3c79625bda880846e0c0
Kroktur <arthur.blanchet.16@gmail.com>
2026-06-22 16:11:07 +0200

    [Kroktur] wtf ca marche ?

--- Assets/Resources/Scripts/TODO/ActivePlayer_List.cs
+++ Assets/Resources/Scripts/TODO/ActivePlayer_List.cs
@@ -10,7 +10,7 @@ public class ActivePlayer_List : Singleton_Obj_Net<ActivePlayer_List>
 
     private readonly SyncList<ulong> PlayerSteamId = new SyncList<ulong>();
 
-    [Command]
+    [Command(requiresAuthority = false)]
     public void CmdAddId(ulong steamId)
     {
         // Sécurité serveur : vérifier que steamId correspond bien à l'appelant
@@ -18,7 +18,7 @@ public class ActivePlayer_List : Singleton_Obj_Net<ActivePlayer_List>
             PlayerSteamId.Add(steamId);
     }
 
-    [Command]
+    [Command(requiresAuthority = false)]
     public void CmdRemove(ulong steamId)
     {
         PlayerSteamId.Remove(steamId);
