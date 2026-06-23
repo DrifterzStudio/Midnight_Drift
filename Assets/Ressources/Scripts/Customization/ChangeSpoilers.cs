7bf8d9ea2c8a97b17a1a859642ee90b3113a4cc1
Brenda-Inocencio <binocencio@gaming.tech>
2026-06-23 09:40:29 +0200

    add change spoilers

---
+++ Assets/Ressources/Scripts/Customization/ChangeSpoilers.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSpoilers : MonoBehaviour {
+
+    public Button spoilersButton;
+
+    public Text spoilersText;
+
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Ressources/Scripts/Customization/ChangeWheels.cs
+++ Assets/Ressources/Scripts/Customization/ChangeWheels.cs
@@ -6,15 +6,29 @@ public class ChangeWheels : MonoBehaviour {
 
     public RCCP_CarController controller;
 
-    public  List<Material> wheelsSets;
+    public List<Transform> wheelsSet;
 
     public Button wheelsButton;
 
     public Text wheelsText;
 
-    private int wheelsSetIndex = -1;
+    private int wheelsSetIndex = 0;
+
+    private void Awake() {
+        wheelsButton.onClick.AddListener(OnButtonClicked);
+    }
 
     void Update() {
-    
+        controller.FrontAxle.leftWheelModel = wheelsSet[wheelsSetIndex];
+        controller.FrontAxle.rightWheelModel = wheelsSet[wheelsSetIndex + 1];
+        controller.RearAxle.leftWheelModel = wheelsSet[wheelsSetIndex + 2];
+        controller.RearAxle.rightWheelModel = wheelsSet[wheelsSetIndex + 3];
+
+        wheelsText.text = "" + wheelsSetIndex / 4;
+    }
+
+    private void OnButtonClicked() {
+        if (wheelsSetIndex + 4 > wheelsSet.Count) wheelsSetIndex = 0;
+        else wheelsSetIndex += 4;
     }
 }
