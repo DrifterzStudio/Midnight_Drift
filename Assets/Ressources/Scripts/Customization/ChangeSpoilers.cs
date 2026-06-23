32cfadcd79f6f961801a81e6fdd1c69c1aa9c8a2
Brenda-Inocencio <binocencio@gaming.tech>
2026-06-23 10:32:19 +0200

    changeSpoilers

--- Assets/Ressources/Scripts/Customization/ChangeSpoilers.cs
+++ Assets/Ressources/Scripts/Customization/ChangeSpoilers.cs
@@ -1,15 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChangeSpoilers : MonoBehaviour {
 
+    public RCCP_CarController controller;
+
     public Button spoilersButton;
 
     public Text spoilersText;
 
+    public List<GameObject> spoilers;
 
-    void Update()
-    {
-        
+    private int spoilersIndex = -1;
+
+    private void Awake() {
+        spoilersButton.onClick.AddListener(OnButtonClicked);
+    }
+
+    void Update() {
+        if (spoilersIndex != -1) controller.Customizer.SpoilerManager.spoilers[spoilersIndex].gameObject.SetActive(true);
+
+        if (spoilersIndex == -1) spoilersText.text = "None";
+        else spoilersText.text = "" + spoilersIndex;
+    }
+
+    private void OnButtonClicked() {
+        if (spoilersIndex != -1) controller.Customizer.SpoilerManager.spoilers[spoilersIndex].gameObject.SetActive(false);
+        if (spoilersIndex + 1 > spoilers.Count) spoilersIndex = -1;
+        else spoilersIndex += 1;
     }
 }
