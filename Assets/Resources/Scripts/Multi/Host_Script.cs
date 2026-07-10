64e1ad91c74abbdadeda74f07673fb9b77b5341a
Arthur Blanchet <arthur.blanchet.16@gmail.com>
2026-07-10 14:04:16 +0200

    [Kroktur] test host disepire

--- Assets/Resources/Scripts/Multi/Host_Script.cs
+++ Assets/Resources/Scripts/Multi/Host_Script.cs
@@ -2,16 +2,22 @@ using System;
 using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class Host_Script : MonoBehaviour
 {
     private bool isReady = false;
     private bool alreadyPresseed = false;
-    
+    [SerializeField] private GameObject startButton;
     private void Start()
     {
         Mirror_Manager.Instance.RegisterPrefab("MultiGameScene", Resources.Load<GameObject>("Prefabs/Multi/PlayerCar"));
         Mirror_Manager.Instance.RegisterPrefab("MultiGameLobbyScene", Resources.Load<GameObject>("Prefabs/Multi/PlayerFps"));
+
+        if (NetworkServer.active)
+        {
+            gameObject.SetActive(false);
+        }
 
     }
     public void OnClick()
@@ -20,6 +26,8 @@ public class Host_Script : MonoBehaviour
             return;
         alreadyPresseed = true;
         Steam_Lobby.Instance.CreateLobby();
+        gameObject.SetActive(false);
+        startButton.SetActive(true);
         isReady = true;
     }
 
